61e04d835a699b9703acdb797e1e2e6563eea86b
Rahul Dalal <rahul.dalal@ibm.com>
2021-07-14 17:17:32 -0700

    corrected deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 docker build -t rahulvdalal/multi-client:latest -t rahulvdalal/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t rahulvdalal/multi-server:latest -t rahulvdalal/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t rahulvdalal/multi-worker:latest -t rahulvdalal/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push rahulvdalal/multi-client:latest -t rahulvdalal/multi-client:$SHA
-docker push rahulvdalal/multi-server:latest -t rahulvdalal/multi-server:$SHA
-docker push rahulvdalal/multi-worker:latest -t rahulvdalal/multi-worker:$SHA
+docker push rahulvdalal/multi-client:latest
+docker push rahulvdalal/multi-server:latest
+docker push rahulvdalal/multi-worker:latest
+docker push rahulvdalal/multi-client:$SHA
+docker push rahulvdalal/multi-server:$SHA
+docker push rahulvdalal/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/client-deployment client=rahulvdalal/multi-client:$SHA
 kubectl set image deployments/server-deployment server=rahulvdalal/multi-server:$SHA
